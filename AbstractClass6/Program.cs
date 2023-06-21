c40685c60b3c13e665bf36fc944254e2b10fb8dc
beverley-dick <beverleydick11@gmail.com>
2023-06-21 11:37:37 +0300

    Add project files.

---
+++ AbstractClass6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AbstractClass6
+{
+   abstract class Animal
+    {
+        protected string name;
+        protected int age;
+        //abstract method
+        public abstract string Name
+        {
+            get;
+            set;
+        }
+        public abstract int Age
+        {
+            get;
+            set;
+        }
+    }
+
+    //inheriting from abstract class
+    class Dog : Animal
+    {
+        //provide implimation of abstract method
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            //create an object of Dog class
+            Dog dog = new Dog();
+            dog.Name = "Tom";
+            dog.Age = 5;
+            Console.WriteLine("Name : " + dog.Name);
+            Console.WriteLine("Age : " + dog.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
